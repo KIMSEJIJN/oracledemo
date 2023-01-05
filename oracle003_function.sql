e00d9c241989b0d7fdad0614e17862ecd20d016a
KIMSEJIJN <120268171+KIMSEJIJN@users.noreply.github.com>
2023-01-05 17:19:48 +0900

    Delete oracle003_function.sql

--- oracle003_function.sql
+++
@@ -1,5 +0,0 @@
-SELECT first_name, salary, CASE WHEN salary <10000 THEN '하'
-                               WHEN salary <20000 THEN '중'
-                               ELSE '상'
-                               ENd AS "구분"
-FROM employees;
